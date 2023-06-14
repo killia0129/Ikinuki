711d668d85cbd50974ddca437e5fc75b0b839a11
YamaokaShinsaku <20010017ys@edu.fca.ac.jp>
2023-06-14 14:51:01 +0900

    いったん保存

--- TPCamera/TPCamera/BossScene.cpp
+++ TPCamera/TPCamera/BossScene.cpp
@@ -1,40 +1,56 @@
 #include "BossScene.h"
 
+const int AfterImageNum = 32;
+const float MainR = 14.f;
+const float CoreR = 7.f;
+const float MaxHP = 2.f;
+const float XSpeed = 0.3f;
+const float YSpeed = 0.4f;
+const float HalfRad = HalfRad;
+const float NinetyDigreeRad = 0.5f;
+const unsigned int DefaultColor = GetColor(0, 255, 0);
+const float MainToBitDiff = 20.f;
+const float QuarterRad = 0.25f;
+const float MainToNeedleDiff = 14.0f;
+const float MainToNeedleTopDiff = 25.f;
+const float TurnSpeed = 1.f;
+const float RoundRad = 2.f;
+
 BossScene::BossScene(VECTOR pos)
 {
     BossScene::pos = pos;
-    for (int i = 0; i < 32; i++)
+    for (int i = 0; i < AfterImageNum; i++)
     {
         prevPos[i] = BossScene::pos;
     }
-    mainR = 14.0f;
-    coreR = 7.0f;
-    HP = 2.0f;
+    mainR = MainR;
+    coreR = CoreR;
+    HP = MaxHP;
     rad = 0.0f;
-    xSpeed = 0.3f;
-    ySpeed = 0.4f;
-    line1Start = VGet(pos.x + mainR * sinf(0.5f * DX_PI_F) * cosf(rad * DX_PI_F), pos.y + mainR * cos(0.5f * DX_PI_F), pos.z + mainR * sinf(0.5f * DX_PI_F) * sinf(rad * DX_PI_F));
-    line1End = VGet(pos.x - mainR * sinf(0.5f * DX_PI_F) * cosf(rad * DX_PI_F), pos.y - mainR * cos(0.5f * DX_PI_F), pos.z - mainR * sinf(0.5f * DX_PI_F) * sinf(rad * DX_PI_F));
-    line2Start = VGet(pos.x + mainR * sinf(0.5f * DX_PI_F) * cosf((rad + 0.5f) * DX_PI_F), pos.y - mainR * cos(0.5f * DX_PI_F), pos.z + mainR * sinf(0.5f * DX_PI_F) * sinf((rad + 0.5f) * DX_PI_F));
-    line2End = VGet(pos.x - mainR * sinf(0.5f * DX_PI_F) * cosf((rad + 0.5f) * DX_PI_F), pos.y + mainR * cos(0.5f * DX_PI_F), pos.z - mainR * sinf(0.5f * DX_PI_F) * sinf((rad + 0.5f) * DX_PI_F));
-    Color = GetColor(0, 255, 0);
+    xSpeed = XSpeed;
+    ySpeed = YSpeed;
+    line1Start = VGet(pos.x + mainR * sinf(HalfRad) * cosf(rad * DX_PI_F), pos.y + mainR * cos(HalfRad), pos.z + mainR * sinf(HalfRad) * sinf(rad * DX_PI_F));
+    line1End = VGet(pos.x - mainR * sinf(HalfRad) * cosf(rad * DX_PI_F), pos.y - mainR * cos(HalfRad), pos.z - mainR * sinf(HalfRad) * sinf(rad * DX_PI_F));
+    line2Start = VGet(pos.x + mainR * sinf(HalfRad) * cosf((rad + NinetyDigreeRad) * DX_PI_F), pos.y - mainR * cos(HalfRad), pos.z + mainR * sinf(HalfRad) * sinf((rad + NinetyDigreeRad) * DX_PI_F));
+    line2End = VGet(pos.x - mainR * sinf(HalfRad) * cosf((rad + NinetyDigreeRad) * DX_PI_F), pos.y + mainR * cos(HalfRad), pos.z - mainR * sinf(HalfRad) * sinf((rad + NinetyDigreeRad) * DX_PI_F));
+    Color = DefaultColor;
     redColorValue = 0;
     bit1Rad = 0.0f;
-    bit2Rad = 0.25f;
+    bit2Rad = QuarterRad;
     bit3Rad = 0.0f;
     bit4Rad = 0.0f;
-    bit1Pos = VGet(pos.x + (20.0f * cosf(bit1Rad * DX_PI_F) * cosf(0.25f * DX_PI_F)), pos.y + (20.0f * cosf(bit1Rad * DX_PI_F) * cosf(0.25f * DX_PI_F)), pos.z - (20.0f * sinf(bit1Rad * DX_PI_F)));
-    bit2Pos = VGet(pos.x - (20.0f * cosf(bit2Rad * DX_PI_F) * cosf(0.25f * DX_PI_F)), pos.y - (20.0f * cosf(bit2Rad * DX_PI_F) * cosf(0.25f * DX_PI_F)), pos.z + (20.0f * sinf(bit2Rad * DX_PI_F)));
-    bit3Pos = VGet(pos.x + (20.0f * cosf(bit3Rad * DX_PI_F)), pos.y, pos.z + (20.0f * sinf(bit3Rad * DX_PI_F)));
-    bit4Pos = VGet(pos.x, pos.y + (20.0f * cosf(bit4Rad * DX_PI_F)), pos.z - (20.0f * sinf(bit4Rad * DX_PI_F)));
-    needle1Pos= VGet(pos.x - (14.0f * cosf(bit1Rad * DX_PI_F) * cosf(0.25f * DX_PI_F)), pos.y - (14.0f * cosf(bit1Rad * DX_PI_F) * cosf(0.25f * DX_PI_F)), pos.z + (14.0f * sinf(bit1Rad * DX_PI_F)));
-    needle2Pos= VGet(pos.x + (14.0f * cosf(bit2Rad * DX_PI_F) * cosf(0.25f * DX_PI_F)), pos.y + (14.0f * cosf(bit2Rad * DX_PI_F) * cosf(0.25f * DX_PI_F)), pos.z - (14.0f * sinf(bit2Rad * DX_PI_F)));
-    needle3Pos= VGet(pos.x - (14.0f * cosf(bit3Rad * DX_PI_F)), pos.y, pos.z - (14.0f * sinf(bit3Rad * DX_PI_F)));
-    needle4Pos = VGet(pos.x, pos.y - (14.0f * cosf(bit4Rad * DX_PI_F)), pos.z + (14.0f * sinf(bit4Rad * DX_PI_F)));
-    needleTop1Pos = VGet(pos.x - (25.0f * cosf(bit1Rad * DX_PI_F) * cosf(0.25f * DX_PI_F)), pos.y - (25.0f * cosf(bit1Rad * DX_PI_F) * cosf(0.25f * DX_PI_F)), pos.z + (25.0f * sinf(bit1Rad * DX_PI_F)));
-    needleTop2Pos = VGet(pos.x + (25.0f * cosf(bit2Rad * DX_PI_F) * cosf(0.25f * DX_PI_F)), pos.y + (25.0f * cosf(bit2Rad * DX_PI_F) * cosf(0.25f * DX_PI_F)), pos.z - (25.0f * sinf(bit2Rad * DX_PI_F)));
-    needleTop3Pos = VGet(pos.x - (25.0f * cosf(bit3Rad * DX_PI_F)), pos.y, pos.z - (25.0f * sinf(bit3Rad * DX_PI_F)));
-    needleTop4Pos = VGet(pos.x, pos.y - (25.0f * cosf(bit4Rad * DX_PI_F)), pos.z + (25.0f * sinf(bit4Rad * DX_PI_F)));
+    bit1Pos = VGet(pos.x + (MainToBitDiff * cosf(bit1Rad * DX_PI_F) * cosf(QuarterRad * DX_PI_F)), pos.y + (MainToBitDiff * cosf(bit1Rad * DX_PI_F) * cosf(QuarterRad * DX_PI_F)), pos.z - (MainToBitDiff * sinf(bit1Rad * DX_PI_F)));
+    bit2Pos = VGet(pos.x - (MainToBitDiff * cosf(bit2Rad * DX_PI_F) * cosf(QuarterRad * DX_PI_F)), pos.y - (MainToBitDiff * cosf(bit2Rad * DX_PI_F) * cosf(QuarterRad * DX_PI_F)), pos.z + (MainToBitDiff * sinf(bit2Rad * DX_PI_F)));
+    bit3Pos = VGet(pos.x + (MainToBitDiff * cosf(bit3Rad * DX_PI_F)), pos.y, pos.z + (MainToBitDiff * sinf(bit3Rad * DX_PI_F)));
+    bit4Pos = VGet(pos.x, pos.y + (MainToBitDiff * cosf(bit4Rad * DX_PI_F)), pos.z - (MainToBitDiff * sinf(bit4Rad * DX_PI_F)));
+    needle2Pos= VGet(pos.x + (MainToNeedleDiff * cosf(bit2Rad * DX_PI_F) * cosf(QuarterRad * DX_PI_F)), pos.y + (MainToNeedleDiff * cosf(bit2Rad * DX_PI_F) * cosf(QuarterRad * DX_PI_F)), pos.z - (MainToNeedleDiff * sinf(bit2Rad * DX_PI_F)));
+    needle1Pos= VGet(pos.x - (MainToNeedleDiff * cosf(bit1Rad * DX_PI_F) * cosf(QuarterRad * DX_PI_F)), pos.y - (MainToNeedleDiff * cosf(bit1Rad * DX_PI_F) * cosf(QuarterRad * DX_PI_F)), pos.z + (MainToNeedleDiff * sinf(bit1Rad * DX_PI_F)));
+    needle3Pos= VGet(pos.x - (MainToNeedleDiff * cosf(bit3Rad * DX_PI_F)), pos.y, pos.z - (MainToNeedleDiff * sinf(bit3Rad * DX_PI_F)));
+    needle4Pos = VGet(pos.x, pos.y - (MainToNeedleDiff * cosf(bit4Rad * DX_PI_F)), pos.z + (MainToNeedleDiff * sinf(bit4Rad * DX_PI_F)));
+    needleTop1Pos = VGet(pos.x - (MainToNeedleTopDiff * cosf(bit1Rad * DX_PI_F) * cosf(QuarterRad * DX_PI_F)), pos.y - (MainToNeedleTopDiff * cosf(bit1Rad * DX_PI_F) * cosf(QuarterRad * DX_PI_F)), pos.z + (MainToNeedleTopDiff * sinf(bit1Rad * DX_PI_F)));
+    needleTop2Pos = VGet(pos.x + (MainToNeedleTopDiff * cosf(bit2Rad * DX_PI_F) * cosf(QuarterRad * DX_PI_F)), pos.y + (MainToNeedleTopDiff * cosf(bit2Rad * DX_PI_F) * cosf(QuarterRad * DX_PI_F)), pos.z - (MainToNeedleTopDiff * sinf(bit2Rad * DX_PI_F)));
+    needleTop3Pos = VGet(pos.x - (MainToNeedleTopDiff * cosf(bit3Rad * DX_PI_F)), pos.y, pos.z - (MainToNeedleTopDiff * sinf(bit3Rad * DX_PI_F)));
+    needleTop4Pos = VGet(pos.x, pos.y - (MainToNeedleTopDiff * cosf(bit4Rad * DX_PI_F)), pos.z + (MainToNeedleTopDiff * sinf(bit4Rad * DX_PI_F)));
 }
 
 BossScene::~BossScene()
@@ -43,13 +59,13 @@ BossScene::~BossScene()
 
 void BossScene::Update(float deltaTime)
 {
-    for (int i = 31; i >= 1; i--)
+    for (int i = AfterImageNum - 1; i >= 1; i--)
     {
         prevPos[i] = prevPos[i - 1];
     }
     prevPos[0] = pos;
-    rad += 1.0f * deltaTime;
-    if (rad >= 2.0f)
+    rad += TurnSpeed * deltaTime;
+    if (rad >= RoundRad)
     {
         rad = 0.0f;
     }
